2f0a3906ec63d5b996ec607930c47bb16e736a53
Victor Belotto <108822052+VictorBelotto@users.noreply.github.com>
2025-02-13 10:09:13 -0300

    new config cors

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 export default function handler(req, res) {
+  res.setHeader("Access-Control-Allow-Origin", "*"); 
+  res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    return res.status(204).end();
+  }
+
   const { cnpj } = req.query;
 
   if (!cnpj) {
@@ -17,6 +25,6 @@ export default function handler(req, res) {
       return res.status(200).json(data);
     })
     .catch(err => {
-      return res.status(500).json({ error: 'Erro ao buscar CNPJ', details: err });
+      return res.status(500).json({ error: 'Erro ao buscar CNPJ', details: err.message });
     });
 }
